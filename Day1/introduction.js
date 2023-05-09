fa9fdad924bc963f8c890d908f4b02d18ebd8a43
Minealtug <200316021@ogr.cbu.edu.tr>
2023-05-09 22:55:25 +0300

    Finished 2.Day

---
+++ Day1/introduction.js
@@ -0,0 +1,15 @@
+// console.log("helllo world")
+console.log( typeof "Mine ALTUĞ");
+console.log( typeof true);
+console.log(typeof 120);
+console.log (typeof null);
+console.log( typeof Object)
+
+let channelName="Prototurk"
+let channelAge=11
+let isChannelStillActive=true
+let secondChannel=null
+let channelMoney=undefined
+
+
+console.log(channelName)
---
+++ Day2/script.js
@@ -0,0 +1,32 @@
+let numOne=3
+let numTwo=3
+//console.log( numOne ==numTwo)
+
+
+
+//Case sensitive 
+let js="Javascript"
+let jsS="javascript"
+//console.log(js==jsS)
+
+let py= "Python"
+
+ let nums=[1,2,3,6,5,4,8]
+let array1=["mine ",13,false,[],undefined]
+
+let nums1=[1,2,3]
+let nums2=[1,2,3]
+
+///console.log(nums1==nums2)
+
+let name ="mi"
+let surname="altuğ"
+
+let fullname=name + surname
+
+console.log(fullname)
+let dfdfd=33
+
+let dfdfd=2
+
+console.log(dfdfd)
